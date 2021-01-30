02ced1d607a626b6b6bbd9689c96d96282c789d2
Teerasej <teerasej@gmail.com>
2021-01-30 20:52:15 +0700

    import and create instance of notification plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() {
   runApp(MyApp());
